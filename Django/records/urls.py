15c09c7df100d023a5e184caad534ce358d8d002
ottaliam <ottaliam@gmail.com>
2023-12-21 09:59:09 +0800

    FoodRecord Figure

--- Django/records/urls.py
+++ Django/records/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("record/addFoodRecord", views.addFoodRecord, name="add_food_record"),
     path("record/addUricacidRecord", views.addUricacidRecord, name="add_uricacid_record"),
-    path("record/addFlareupRecord", views.addFlareupRecord, name="add_flareup_record")
+    path("record/addFlareupRecord", views.addFlareupRecord, name="add_flareup_record"),
+
+    path("record/chartFoodRecord", views.computeStatisticsAndChartsForFoodRecord, name="chart_food_record"),
 ]
--- Django/records/views.py
+++ Django/records/views.py
@@ -1,5 +1,9 @@
+import os
 import json
 import datetime
+import tempfile
+import matplotlib.pyplot as plt
+from collections import defaultdict
 
 from django.http import JsonResponse
 from django.utils.dateparse import parse_date
@@ -7,6 +11,22 @@ from django.utils.dateparse import parse_date
 from user.models import User
 from food.models import Food
 from .models import FoodRecord, UricacidRecord, FlareupRecord
+from django.conf import settings
+
+# ---------------------------------------- Utils ----------------------------------------------
+def generate_and_save_chart(data, title, temp_dir, date_format):
+    plt.figure()
+    dates = [date.strftime(date_format) for date in data.keys()]
+    plt.plot(dates, data.values())
+    plt.title(title)
+    plt.xlabel('Date')
+    plt.ylabel('Purine Intake')
+
+    temp_file = tempfile.NamedTemporaryFile(dir=temp_dir, suffix='.png', delete=False)
+    plt.savefig(temp_file.name)
+    plt.close()
+
+    return os.path.join(settings.MEDIA_URL, 'temp', os.path.basename(temp_file.name))
 
 # ----------------------------------------- Food Record -----------------------------------------
 
@@ -35,6 +55,52 @@ def addFoodRecord(request):
     else:
         return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
 
+def computeStatisticsAndChartsForFoodRecord(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        record_date = datetime.datetime.strptime(data.get('date'), '%Y-%m-%d').date()
+        temp_dir = os.path.join(settings.MEDIA_ROOT, 'temp')
+        if not os.path.exists(temp_dir):
+            os.makedirs(temp_dir)
+
+        # Define time ranges based on given date
+        last_7_days = [record_date - datetime.timedelta(days=i) for i in range(7)]
+        last_month_end = record_date.replace(day=1) - datetime.timedelta(days=1)
+        last_month_start = last_month_end.replace(day=1)
+        last_month = [last_month_start + datetime.timedelta(days=i) for i in range((last_month_end - last_month_start).days + 1)]
+        last_year_start = record_date.replace(year=record_date.year-1, month=record_date.month+1, day=1)
+        last_year = [last_year_start + datetime.timedelta(days=i) for i in range((record_date - last_year_start).days + 1)]
+
+        # Fetch records and compute statistics
+        statistics = {
+            'last_7_days': defaultdict(float), 'last_month': defaultdict(float), 'last_year': defaultdict(float)
+        }
+
+        for date in last_7_days:
+            records = FoodRecord.objects.filter(record_date=date)
+            statistics['last_7_days'][date] += sum([r.quantity * r.food.purine_per_unit for r in records])
+
+        for date in last_month:
+            records = FoodRecord.objects.filter(record_date=date)
+            statistics['last_month'][date] += sum([r.quantity * r.food.purine_per_unit for r in records])
+
+        for month in range(1, 13):
+            month_start = record_date.replace(month=month, day=1)
+            month_end = (month_start + datetime.timedelta(days=32)).replace(day=1) - datetime.timedelta(days=1)
+            records = FoodRecord.objects.filter(record_date__range=[month_start, month_end])
+            month_key = month_start.strftime('%Y-%m')
+            statistics['last_year'][month_key] += sum([r.quantity * r.food.purine_per_unit for r in records])
+
+        # Generate and save charts
+        charts = {
+            'last_7_days_chart': generate_and_save_chart(statistics['last_7_days'], 'Purine Intake: Last 7 Days', temp_dir, '%m-%d'),
+            'last_month_chart': generate_and_save_chart(statistics['last_month'], 'Purine Intake: Last Month', temp_dir, '%m-%d'),
+            'last_year_chart': generate_and_save_chart(statistics['last_year'], 'Purine Intake: Last Year', temp_dir, '%Y-%m')
+        }
+
+        return JsonResponse({'status': 'success', 'charts': charts})
+    else:
+        return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
 
 def getFoodRecordsForDate(request):
     if request.method == 'POST':
